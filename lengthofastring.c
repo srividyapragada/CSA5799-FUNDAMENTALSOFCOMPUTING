d0dc69c2a7a199daae5f7ca93d04635e64fb9b1f
srividyapragada <126004644+srividyapragada@users.noreply.github.com>
2023-08-10 10:25:20 +0530

    Add files via upload

---
+++ lengthofastring.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    printf("The length of the string is: %d\n", len);
+}
